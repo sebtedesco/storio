198f2c851317d14ea0bc179f2a730a7e9130b11c
Peter Han <peterjhan88@gmail.com>
2020-01-22 18:30:36 -0800

    PM approved portion/component

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import ListingDetail from './listing-detail';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       message: null,
-      isLoading: true
+      isLoading: true,
+      view: 'listing-detail'
     };
   }
 
@@ -18,8 +20,23 @@ export default class App extends React.Component {
   }
 
   render() {
-    return this.state.isLoading
-      ? <h1>Testing connections...</h1>
+    const hardCodedData01 = {
+      userId: 1,
+      firstName: 'Brian',
+      lastName: 'Wilson',
+      profilePicturePath: './images/users/brian-wilson.jpg',
+      storagePicturePath: './images/storages/car-garage.jpg',
+      city: 'Laguna Beach',
+      state: 'CA',
+      title: 'Clean space in lodge of garage',
+      longDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur luctus justo est, quis posuere lorem ultricies vitae. Sed ut turpis posuere, laoreet diam id, lacinia nisl. Curabitur nec est a metus blandit lobortis.',
+      maxValue: 1000000,
+      pricePerDay: 300
+    };
+    return this.state.view === 'listing-detail'
+      ? <ListingDetail
+        data={hardCodedData01}
+      />
       : <h1>{ this.state.message }</h1>;
   }
 }
---
+++ client/components/listing-detail.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export default class ListingDetail extends React.Component {
+  render() {
+    const data = this.props.data;
+    return (
+      <div className='col-12 list-detail my-auto'>
+        <div className='col-12 storage-image-container'>
+          <img
+            className='list-detail-storage-image'
+            src={data.storagePicturePath}
+            alt='storage-image should be here'
+          />
+        </div>
+        <div className='list-detail-title'>{data.title}</div>
+        <div className='location-and-user-profile'>
+          <div className='list-detail-address'>{`${data.city}, ${data.state}`}</div>
+          <div className='list-detail-host-profile'>
+            <div className='host-image-container'>
+              <img
+                className='list-detail-host-picture'
+                src={data.profilePicturePath}
+                alt='host-profile-image should be here'
+              />
+            </div>
+            <div className='list-detail-host-full-name'>{`${data.firstName} ${data.lastName}`}</div>
+          </div>
+        </div>
+        <div className='storage-details'>
+          <div className='long-description-title'>About the space</div>
+          <div className='long-description'>{data.longDescription}</div>
+          <div className='max-coverage'>
+            <div className='list-detail-max-coverage-title'>Maximum value allowed: </div>
+            <div className='max-value'>{` $ ${(data.maxValue / 100)}`}</div>
+          </div>
+          <div className='price-container'>
+            <div className='list-detail-price'>Price:</div>
+            <div>{`$${(data.pricePerDay * 7 / 100).toFixed(2)}/wk, $${(data.pricePerDay * 30 / 100).toFixed(2)}/month`}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
