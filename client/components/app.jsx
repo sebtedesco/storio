80b7b5f4c56f8d250b0e1a44daf9e3dc98400a38
Peter Han <peterjhan88@gmail.com>
2020-01-23 10:30:26 -0800

    Original codce in app.jsx is restored. Incomplete routing feature commented out in app.jsx Finished body component of list-detail

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import ListingDetail from './listing-detail';
+
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+// import ListingDetail from './listing-detail';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       message: null,
-      isLoading: true,
-      view: 'listing-detail'
+      isLoading: true
     };
   }
 
@@ -20,23 +22,41 @@ export default class App extends React.Component {
   }
 
   render() {
-    const hardCodedData01 = {
-      userId: 1,
-      firstName: 'Brian',
-      lastName: 'Wilson',
-      profilePicturePath: './images/users/brian-wilson.jpg',
-      storagePicturePath: './images/storages/car-garage.jpg',
-      city: 'Laguna Beach',
-      state: 'CA',
-      title: 'Clean space in lodge of garage',
-      longDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur luctus justo est, quis posuere lorem ultricies vitae. Sed ut turpis posuere, laoreet diam id, lacinia nisl. Curabitur nec est a metus blandit lobortis.',
-      maxValue: 1000000,
-      pricePerDay: 300
-    };
-    return this.state.view === 'listing-detail'
-      ? <ListingDetail
-        data={hardCodedData01}
-      />
-      : <h1>{ this.state.message }</h1>;
+    return this.state.isLoading
+      ? <h1>Testing connections...</h1>
+      : <h1>{this.state.message}</h1>;
   }
+
+  // render() {
+  //   return (
+  //     <Router>
+  //       <Route exact={true} path='/' render={() => (
+  //         <h1>{this.state.message}</h1>
+  //         // <Link to={'/landing'}>{'To landing Page'}</Link>
+  //       )} />
+  //       <Route exact={true} path='/landing' render={() => (
+  //         <h1>This is landing page</h1>
+  //       )} />
+  //       <Route path='/listing-detail' component={ListingDetailFunc}/>
+  //     </Router>
+  //   );
+  // }
 }
+
+// Peter
+// const ListingDetailFunc = () => {
+//   const hardCodedData01 = {
+//     userId: 1,
+//     firstName: 'Brian',
+//     lastName: 'Wilson',
+//     profilePicturePath: './images/users/brian-wilson.jpg',
+//     storagePicturePath: './images/storages/car-garage.jpg',
+//     city: 'Laguna Beach',
+//     state: 'CA',
+//     title: 'Clean space in lodge of garage',
+//     longDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur luctus justo est, quis posuere lorem ultricies vitae. Sed ut turpis posuere, laoreet diam id, lacinia nisl. Curabitur nec est a metus blandit lobortis.',
+//     maxValue: 1000000,
+//     pricePerDay: 300
+//   };
+//   return (<ListingDetail data={hardCodedData01} />);
+// };
