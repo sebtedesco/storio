2f51d9b854132daf145dee5f693bb49ea7709e0f
Christopher Ward <chrsward90@gmail.com>
2020-01-23 10:27:07 -0800

    current progress not done

--- server/index.js
+++ server/index.js
@@ -19,6 +19,11 @@ app.get('/api/health-check', (req, res, next) => {
     .catch(err => next(err));
 });
 
+// app.post('/api/listings/', (req, res, next) => {
+//   const sql = `
+//   insert into`;
+// });
+
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });
