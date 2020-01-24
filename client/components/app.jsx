c1f4ccdfcbad59c4f87b97c5e96e1cb842e2129a
Peter Han <peterjhan88@gmail.com>
2020-01-23 16:08:53 -0800

    Link is included And routing will be formatted in more friendly format

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-// import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import Conversations from './Conversations';
 import CreateAccount from './CreateAccount';
 import ExploreList from './ExploreList';
@@ -19,6 +18,8 @@ export default class App extends React.Component {
       message: null,
       isLoading: true
     };
+    this.allLinks = this.allLinks.bind(this);
+    this.searchLocation = this.searchLocation.bind(this);
   }
 
   componentDidMount() {
@@ -29,20 +30,64 @@ export default class App extends React.Component {
       .finally(() => this.setState({ isLoading: false }));
   }
 
+  allLinks() {
+    return (
+      <div className='d-flex flex-column col-11 mx-2 align-items-center'>
+        <Link to='/'>To Home Page</Link>
+        <Link to='/conversations'>To Conversations</Link>
+        <Link to='/create-account'>To CreateAccount</Link>
+        <Link to='/explore-list'>To ExploreList</Link>
+        <Link to='/explore-map'>To ExploreMap</Link>
+        <Link to='/host-listings'>To HostListings</Link>
+        <Link to='/listing-detail'>To Listing Detail</Link>
+        <Link to='/log-in'>To LogInPage</Link>
+        <Link to='/message'>To Message</Link>
+        <Link to='/search'>To Search</Link>
+      </div>
+    );
+  }
+
+  searchLocation(some) {
+    // eslint-disable-next-line no-console
+    console.log(some);
+  }
+
   render() {
     return (
       <Router>
-        <Route exact={true} path='/' render={() => (<h1>{this.state.message}</h1>)} />
-        <Route exact={true} path='/conversations' component={Conversations} />
-        <Route exact={true} path='/create-account' component={CreateAccount} />
-        <Route exact={true} path='/explore-list' component={ExploreList} />
-        <Route exact={true} path='/explore-map' component={ExploreMap} />
-        <Route exact={true} path='/host-listings' component={HostListings} />
-        <Route exact={true} path='/landing-page' component={LandingPage} />
-        <Route exact={true} path='/listing-detail' component={ListingDetail}/>
-        <Route exact={true} path='/log-in' component={LogInPage} />
-        <Route exact={true} path='/message' component={Message} />
-        <Route exact={true} path='/search' component={Search} />
+        <Route exact={true} path='/' >
+          <LandingPage searchLocation={this.searchLocation} />
+        </Route>
+        <Route exact={true} path='/conversations'>
+          <Conversations />
+        </Route>
+        <Route exact={true} path='/create-account'>
+          <CreateAccount />
+        </Route>
+        <Route exact={true} path='/explore-list'>
+          <ExploreList />
+        </Route>
+        <Route exact={true} path='/explore-map'>
+          <ExploreMap />
+        </Route>
+        <Route exact={true} path='/host-listings'>
+          <HostListings />
+        </Route>
+        <Route exact={true} path='/listing-detail'>
+          <ListingDetail />
+        </Route>
+        <Route exact={true} path='/log-in'>
+          <LogInPage />
+        </Route>
+        <Route exact={true} path='/message' >
+          <Message />
+        </Route>
+        <Route exact={true} path='/search'>
+          <Search searchLocation={this.searchLocation}/>
+        </Route>
+        <Route path='/' >
+          {this.allLinks}
+        </Route>
       </Router>
     );
   }
