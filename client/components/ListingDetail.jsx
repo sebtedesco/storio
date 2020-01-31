0f131a1f0cffcbf96967b2fbd36a72e7c62ea9b2
sebtedesco <stedescoc@gmail.com>
2020-01-31 00:14:26 +0000

    changed back button on details page

--- client/components/ListingDetail.jsx
+++ client/components/ListingDetail.jsx
@@ -35,57 +35,64 @@ class ListingDetail extends React.Component {
       );
     }
     return (
-
-      <div className='col-12 list-detail my-auto'>
-        <div className="col-12 text-muted" onClick={() => { this.props.history.goBack(); }}>
-             &lt; Back
-        </div>
-        <div className="col-12 storage-image-container">
-          <img
-            className="list-detail-storage-image"
-            src={data.storagePicturePath}
-            alt="storage-image should be here"
-          />
-        </div>
-        <div className="list-detail-title">{data.title}</div>
-        <div className="location-and-user-profile">
-          <div className="list-detail-address">{`${data.city}, ${data.state}`}</div>
-          <div className="list-detail-host-profile">
-            <div className="host-image-container">
-              <img
-                className="list-detail-host-picture"
-                src={data.profilePicturePath}
-                alt="host-profile-image should be here"
-              />
+      <div className="container-fluid detail-container">
+        <div className="container col-12 list-detail my-auto m-0">
+          <i
+            className="far fa-arrow-alt-circle-left col-12 back-arrow"
+            onClick={() => {
+              this.props.history.goBack();
+            }}
+          ></i>
+          {/* </div> */}
+          <div className="col-12 storage-image-container">
+            <img
+              className="list-detail-storage-image"
+              src={data.storagePicturePath}
+              alt="storage-image should be here"
+            />
+          </div>
+          <div className="list-detail-title">{data.title}</div>
+          <div className="location-and-user-profile">
+            <div className="list-detail-address">{`${data.city}, ${data.state}`}</div>
+            <div className="list-detail-host-profile">
+              <div className="host-image-container">
+                <img
+                  className="list-detail-host-picture"
+                  src={data.profilePicturePath}
+                  alt="host-profile-image should be here"
+                />
+              </div>
+              <div className="list-detail-host-full-name">{`${data.firstName} ${data.lastName}`}</div>
             </div>
-            <div className="list-detail-host-full-name">{`${data.firstName} ${data.lastName}`}</div>
           </div>
-        </div>
-        <div className="storage-details">
-          <div className="long-description-title">About the space</div>
-          <div className="long-description">{data.longDescription}</div>
-          <div className="max-coverage">
-            <div className="list-detail-max-coverage-title">
-              Maximum value allowed:{' '}
+          <div className="storage-details">
+            <div className="long-description-title">About the space</div>
+            <div className="long-description">{data.longDescription}</div>
+            <div className="max-coverage">
+              <div className="list-detail-max-coverage-title">
+                Maximum value allowed:{' '}
+              </div>
+              <div className="max-value">{` $ ${data.maxValue / 100}`}</div>
             </div>
-            <div className="max-value">{` $ ${data.maxValue / 100}`}</div>
+            <div className="price-container">
+              <div className="list-detail-price">Price:</div>
+              <div>{`$${((data.pricePerDay * 7) / 100).toFixed(2)}/wk, $${(
+                (data.pricePerDay * 30) /
+                100
+              ).toFixed(2)}/month`}</div>
+            </div>
           </div>
-          <div className="price-container">
-            <div className="list-detail-price">Price:</div>
-            <div>{`$${((data.pricePerDay * 7) / 100).toFixed(2)}/wk, $${(
-              (data.pricePerDay * 30) /
-              100
-            ).toFixed(2)}/month`}</div>
+          <div className="form-row col-12 d-flex justify-content-center detail-button-row">
+            <Link to={`/message/${this.props.user.userId}/${data.hostId}`}>
+              <button
+                className={`btn listing-button ${
+                  this.state.isMessageButtonVisible === true ? '' : 'd-none'
+                }`}
+              >
+                Message Host
+              </button>
+            </Link>
           </div>
-        </div>
-        <div className="form-row col-12 d-flex justify-content-center detail-button-row">
-          <Link to={`/message/${this.props.user.userId}/${data.hostId}`}>
-            <button
-              className={`btn listing-button ${this.state.isMessageButtonVisible === true ? '' : 'd-none'}`}
-            >
-              Message Host
-            </button>
-          </Link>
         </div>
       </div>
     );
